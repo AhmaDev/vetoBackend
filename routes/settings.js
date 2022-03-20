a0665f6b159f31d2001b4dec765f6d0b34e49fd0
AhmaDev <ahmadwig@gmail.com>
2022-03-20 19:13:55 +0300

    Add app version route

--- routes/settings.js
+++ routes/settings.js
@@ -4,13 +4,19 @@ var mysql = require('mysql');
 var db = require('../config/database');
 var connection = mysql.createConnection(db);
 
-router.get('/', function(req, res) {
-    connection.query('SELECT * FROM settings', function(err,result) {
+router.get('/', function (req, res) {
+    connection.query('SELECT * FROM settings', function (err, result) {
         res.send(result)
     })
 })
 
-router.put('/', function(req, res) {
+router.get('/appVersion', function (req, res) {
+    connection.query('SELECT * FROM settings WHERE variable = "appVersion"', function (err, result) {
+        res.send({ version: result[0].value })
+    })
+})
+
+router.put('/', function (req, res) {
     connection.query('UPDATE settings SET value = ? WHERE variable = "title"', [req.body.title]);
     connection.query('UPDATE settings SET value = ? WHERE variable = "workStartTime"', [req.body.workStartTime]);
     connection.query('UPDATE settings SET value = ? WHERE variable = "workEndTime"', [req.body.workEndTime]);
