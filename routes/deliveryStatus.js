fe2fdefd88ea2a015faa38984c29c2d039f97d88
AhmaDev <ahmadwig@gmail.com>
2022-03-20 15:08:55 +0300

    Fixes

--- routes/deliveryStatus.js
+++ routes/deliveryStatus.js
@@ -36,8 +36,9 @@ router.post('/', function (req, res, next) {
 });
 
 router.post('/multipleInsert', function (req, res, next) {
-    for (let i = 0; i < req.body.deliveries.length; i++) {
-        connection.query(`SELECT * FROM deliveryDelegates WHERE deliveryId = ${req.body.deliveries[i]}`, (deliveriesErr, deliveriesResult) => {
+    if (req.body.deliveryStatusType == 1) {
+        var deliveryIds = JSON.stringify(req.body.map((e) => e.deliveries)).slice(1, -1);
+        connection.query(`SELECT * FROM deliveryDelegates WHERE deliveryId IN (${deliveryIds})`, (deliveriesErr, deliveriesResult) => {
             console.log(deliveriesErr);
             var delegatesIds = JSON.stringify(deliveriesResult.map((e) => e.delegateId)).slice(1, -1);
             connection.query(`SELECT invoiceContent.itemId, SUM(count) As count, SUM(total) As total, invoiceContent.discountTypeId, invoice.createdBy, invoice.sellPriceId, invoice.invoiceTypeId , (SELECT itemName FROM item WHERE idItem = invoiceContent.itemId) As itemName FROM invoiceContent JOIN invoice ON invoiceContent.invoiceId = invoice.idInvoice WHERE invoice.invoiceTypeId = 1 AND invoice.createdBy IN (${delegatesIds}) AND DATE(invoice.createdAt) = '${req.body.date}' AND invoiceContent.count != 0 GROUP BY invoiceContent.itemId, invoiceContent.discountTypeId ORDER BY invoiceContent.itemId , invoiceContent.discountTypeId`, (err, result) => {
@@ -60,8 +61,36 @@ router.post('/multipleInsert', function (req, res, next) {
                 });
             })
         });
+        res.sendStatus(200);
+    } else {
+
+        for (let i = 0; i < req.body.deliveries.length; i++) {
+            connection.query(`SELECT * FROM deliveryDelegates WHERE deliveryId = ${req.body.deliveries[i]}`, (deliveriesErr, deliveriesResult) => {
+                console.log(deliveriesErr);
+                var delegatesIds = JSON.stringify(deliveriesResult.map((e) => e.delegateId)).slice(1, -1);
+                connection.query(`SELECT invoiceContent.itemId, SUM(count) As count, SUM(total) As total, invoiceContent.discountTypeId, invoice.createdBy, invoice.sellPriceId, invoice.invoiceTypeId , (SELECT itemName FROM item WHERE idItem = invoiceContent.itemId) As itemName FROM invoiceContent JOIN invoice ON invoiceContent.invoiceId = invoice.idInvoice WHERE invoice.invoiceTypeId = 1 AND invoice.createdBy IN (${delegatesIds}) AND DATE(invoice.createdAt) = '${req.body.date}' AND invoiceContent.count != 0 GROUP BY invoiceContent.itemId, invoiceContent.discountTypeId ORDER BY invoiceContent.itemId , invoiceContent.discountTypeId`, (err, result) => {
+                    console.log(err);
+                    connection.query(`SELECT * FROM invoice WHERE invoiceTypeId = 1 AND createdBy IN (${delegatesIds}) AND DATE(invoice.createdAt) = '${req.body.date}'`, (errInvoices, resultInvoices) => {
+                        console.log(errInvoices);
+                        if (result.length > 0) {
+                            connection.query("INSERT INTO deliveryStatus SET ?", {
+                                deliveryId: req.body.deliveries[i],
+                                delegates: JSON.stringify(deliveriesResult.map((e) => e.delegateId)),
+                                invoicesData: JSON.stringify(result),
+                                createdAt: req.body.date,
+                                invoices: JSON.stringify(resultInvoices.map((e) => e.idInvoice)),
+                                notice: "none",
+                                deliveryStatusType: req.body.deliveryStatusType,
+                            }, (err3, result3) => {
+                                console.log(err3, result3);
+                            });
+                        }
+                    });
+                })
+            });
+        }
+        res.sendStatus(200);
     }
-    res.sendStatus(200);
 });
 
 
