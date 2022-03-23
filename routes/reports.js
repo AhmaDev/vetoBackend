8af65a7bec3f9a26ee6ac26057444158d047817d
AhmaDev <ahmadwig@gmail.com>
2022-03-23 17:37:50 +0300

    Fixes

--- routes/reports.js
+++ routes/reports.js
@@ -43,6 +43,7 @@ router.get('/delegateItems/:id', function (req, res, next) {
       console.log(err);
       res.sendStatus(500);
     } else {
+      result = result.sort((a,b) => b.totalPrice > a.totalPrice);
       res.send(result);
     }
   })
