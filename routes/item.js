246bba52c2638af625f94a33de14de519430f58c
AhmaDev <ahmadwig@gmail.com>
2024-11-01 15:18:36 +0300

    fix partitions

--- routes/item.js
+++ routes/item.js
@@ -166,7 +166,7 @@ router.get("/compressedDetailedStore", function (req, res, next) {
             'sell',IFNULL(SUM(CASE WHEN invoice.invoiceTypeId = 1 THEN count ELSE 0 END),0),
             'buy',IFNULL(SUM(CASE WHEN invoice.invoiceTypeId = 2 THEN count ELSE 0 END),0),
             'restore',IFNULL(SUM(CASE WHEN invoice.invoiceTypeId = 3 THEN count ELSE 0 END),0)
-        ) FROM invoiceContent PARTITION (p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23) JOIN invoice ON invoiceContent.invoiceId = invoice.idInvoice WHERE invoiceContent.invoiceId IN (SELECT idInvoice FROM invoice WHERE invoice.createdAt BETWEEN '${date1} 00:00:00' AND '${date2} 23:59:59') AND invoiceContent.itemId = item.idItem) As stocks FROM item WHERE item.isAvailable = 1`,
+        ) FROM invoiceContent PARTITION (p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24,p25,p26,p27,p28,p29,p30,p31,p32,p33,p34,p35) JOIN invoice ON invoiceContent.invoiceId = invoice.idInvoice WHERE invoiceContent.invoiceId IN (SELECT idInvoice FROM invoice WHERE invoice.createdAt BETWEEN '${date1} 00:00:00' AND '${date2} 23:59:59') AND invoiceContent.itemId = item.idItem) As stocks FROM item WHERE item.isAvailable = 1`,
       (err, result) => {
         res.send(result);
         if (err) {
